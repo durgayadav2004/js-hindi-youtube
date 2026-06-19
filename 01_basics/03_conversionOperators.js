1d5d2252cb79407c9aa6cefb384c75c1176bae70
durgayadav2004 <168621482+durgayadav2004@users.noreply.github.com>
2026-06-19 10:27:09 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,20 @@
+let score=true
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+// "33"=33
+// "33abc"=NaN
+// true= 1
+//false=0
+//null =0
+//undefined = NaN
+let isLoggedIn = 1
+let booleanIsLoggedIn =Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn)
+//1= true,0=false,empty string= false,"hitesh"=true
+let somenumber=33
+let stringsomenumber =String(somenumber)
+console.log(stringsomenumber)
+console.log(typeof stringsomenumber)
